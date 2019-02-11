890b8dc5f29671a7c4831b89c4d3c7d3dad23d06
c10t <c10t@fa.ke>
2019-02-12 00:13:57 +0900

    this resolves the issue that 'npm run dev' stopped

--- front-nuxt/app/components/TheHeader.vue
+++ front-nuxt/app/components/TheHeader.vue
@@ -2,20 +2,9 @@
 <el-menu mode="horizontal" :router="true">
   <el-menu-item index="1" style="pointer-events:none;"> Nuxt News App </el-menu-item>
   <el-menu-item index="2" :route="{ path: '/posts/' }"> Posts </el-menu-item>
-  
   <no-ssr>
     <el-menu-item index="4" style="float: right;" :route="{ path: '/' }"><span> Sign In </span></el-menu-item>
   </no-ssr>
-  <el-menu-item index="5" style="float: right;" :route="{ path: '/posts/new' }"><span>+ New<span></el-menu-item>
+  <el-menu-item index="5" style="float: right;" :route="{ path: '/posts/new' }">New Post</el-menu-item>
 </el-menu>
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
